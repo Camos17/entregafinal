c8e3b8067701980363ed4e2c93c0296857039faa
Camilo Castro <camos8710@gmail.com>
2022-12-17 21:04:57 -0500

    First migration to MVC - Finisehd Dentist controller and impl, add create, searchAll, update, delete methods

--- entregafinal/src/main/java/com/entregafinalspring/entregafinal/servicesNew/impl/DentistServiceImpl.java
+++ entregafinal/src/main/java/com/entregafinalspring/entregafinal/servicesNew/impl/DentistServiceImpl.java
@@ -6,10 +6,10 @@ import com.entregafinalspring.entregafinal.servicesNew.DentistService;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class DentistServiceImpl implements DentistService {
@@ -29,7 +29,7 @@ public class DentistServiceImpl implements DentistService {
     // CONSTANTES PARA MANIPULACION BD
     private final static String CREATE_DENTIST = "CREATE TABLE IF NOT EXISTS dentists" +
             " (id int auto_increment primary key, name varchar(255), lastname varchar(255), registration int)";
-    private final static String INSERT_DENTIST_DATA = "INSERT INTO DENTISTS (id, name, lastname, registration) VALUES(?, ?, ?, ?)";
+    private final static String INSERT_DENTIST_DATA = "INSERT INTO DENTISTS (name, lastname, registration) VALUES(?, ?, ?)";
     private final static String SELECT_DENTIST_DATA = "SELECT id, name, lastname, registration FROM DENTISTS WHERE id = ?";
 
     private final static String SELECT_ALL_DENTISTS = "SELECT * FROM DENTISTS";
@@ -39,7 +39,7 @@ public class DentistServiceImpl implements DentistService {
     private final static String DELETE_DENTIST = "DELETE FROM DENTISTS WHERE id = ?";
 
     @Override
-    public void createDentist(Dentist dentist) throws SQLException {
+    public Dentist createDentist(Dentist dentist) throws SQLException {
         logger.debug("Guardando un nuevo odontologo -1");
         Connection connection = null;
         DriverManager driverManager = null;
@@ -57,10 +57,10 @@ public class DentistServiceImpl implements DentistService {
             connection.setAutoCommit(false);
 
             psInsert = connection.prepareStatement(INSERT_DENTIST_DATA);
-            psInsert.setInt(1, dentist.getId());
-            psInsert.setString(2, dentist.getName());
-            psInsert.setString(3, dentist.getLastname());
-            psInsert.setString(4, dentist.getRegistration());
+//            psInsert.setInt(1, dentist.getId()); // Este ya no debe ir porque el id del insert esta auto incremental
+            psInsert.setString(1, dentist.getName());
+            psInsert.setString(2, dentist.getLastname());
+            psInsert.setString(3, dentist.getRegistration());
 
             psInsert.executeUpdate();
             connection.commit();
@@ -73,6 +73,117 @@ public class DentistServiceImpl implements DentistService {
         } finally {
             connection.close();
         }
+        return dentist;
     }
+
+    @Override
+    public Dentist searchDentist(int id) {
+        logger.debug("buscando el odontologo con id= " + id);
+        Connection connection = null;
+        PreparedStatement preparedStatement;
+        Dentist dentist = null;
+
+        try {
+            Class.forName(DB_JDBC_DRIVER).getDeclaredConstructor().newInstance();
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+
+            preparedStatement = connection.prepareStatement(SELECT_DENTIST_DATA);
+            preparedStatement.setInt(1, id);
+
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while(rs.next()) {
+                int dentistId = rs.getInt("id");
+                String dentistName = rs.getString("name");
+                String dentistLastname = rs.getString("lastname");
+                String registration = rs.getString("registration");
+
+                dentist = new Dentist(dentistId, dentistName, dentistLastname, registration);
+            }
+            preparedStatement.close();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException |
+                 ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return dentist;
+    }
+
+    @Override
+    public List<Dentist> searchAllDentist() {
+        Connection connection = null;
+        PreparedStatement ps = null;
+        List<Dentist> dentists = new ArrayList<>();
+
+        try {
+            Class.forName(DB_JDBC_DRIVER).getDeclaredConstructor().newInstance();
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+
+            ps = connection.prepareStatement(SELECT_ALL_DENTISTS);
+
+            ResultSet result = ps.executeQuery();
+
+            while (result.next()) {
+                int dentistId = result.getInt("id");
+                String dentistName = result.getString("name");
+                String dentistLastname = result.getString("lastname");
+                String registration = result.getString("registration");
+
+                Dentist dentist = new Dentist(dentistId, dentistName, dentistLastname, registration);
+                dentists.add(dentist);
+            }
+            ps.close();
+        } catch (ClassNotFoundException | InvocationTargetException | InstantiationException | IllegalAccessException |
+                 NoSuchMethodException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return dentists;
+    }
+
+    @Override
+    public Dentist updateDentist(Dentist dentist) {
+        Connection connection = null;
+        PreparedStatement ps = null;
+
+        try {
+            Class.forName(DB_JDBC_DRIVER).getDeclaredConstructor().newInstance();
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+
+            ps = connection.prepareStatement(UPDATE_DENTIST);
+            ps.setString(1, dentist.getName());
+            ps.setString(2, dentist.getLastname());
+            ps.setString(3, dentist.getRegistration());
+            ps.setInt(4, dentist.getId());
+
+            ps.executeUpdate();
+            ps.close();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException |
+                 ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return dentist;
+    }
+
+    @Override
+    public void deleteDentist(Integer id) {
+        Connection connection = null;
+        PreparedStatement ps = null;
+
+        try {
+            Class.forName(DB_JDBC_DRIVER).getDeclaredConstructor().newInstance();
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+
+            ps = connection.prepareStatement(DELETE_DENTIST);
+            ps.setInt(1, id);
+
+            ps.executeUpdate();
+            ps.close();
+            logger.debug("Odontologo con id" + id  + " fue elimindao");
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException |
+                 ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
 }
